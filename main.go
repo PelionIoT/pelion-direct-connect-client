e5e3517d02190fd4fe3446a94448e904a800c041
Brian Esquilona <brian.esquilona@arm.com>
2021-06-25 15:01:37 -0500

    Make the server work with multiple clients, enable pings by ping-duration (#5)
    * Keep the tunnel alive by pinging the other side of the tunnel
    
    Keep the tunnel alive by pinging the other side of the tunnel
    
    * fmt -> logrus for logging
    
    Change logging from fmt to logrus library
    
    * Len channel -> bytes channel
    
    Change length channel to bytes channel instead for getting data from tcp to ws
    
    * Make the server work with multiple clients, enable pings by ping-duration
    
    * Add ctx.Done return, add connection details on logs
    
    * Defer cancel, rename inner ctx

--- main.go
+++ main.go
@@ -3,34 +3,41 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"net"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/sirupsen/logrus"
 )
 
 var listenURI string
 var cloudURI string
 var apiKey string
 var applicationID string
-
+var pingDuration int
 func main() {
 	flag.StringVar(&listenURI, "listen-uri", "0.0.0.0:8181", "Local TCP server address")
 	flag.StringVar(&cloudURI, "cloud-uri", "", "Cloud URI that edge tunneling service is running on")
 	flag.StringVar(&apiKey, "api-key", "", "API Key or JWT Token that needs to connect to edge tunneling service")
 	flag.StringVar(&applicationID, "application-id", "", "If JWT is used instead of api-key, application-id is needed")
+	flag.IntVar(&pingDuration, "ping-duration", 5, "Ping duration in seconds, 0 will disable pings to the tunnel")
 	flag.Parse()
 
+	customFormatter := new(logrus.TextFormatter)
+	customFormatter.FullTimestamp = true
+	logrus.SetFormatter(customFormatter)
+	logrus.SetLevel(logrus.DebugLevel)
+
 	if cloudURI == "" {
-		fmt.Printf("cloud-uri must be provided\n")
+		logrus.Errorf("cloud-uri must be provided\n")
 
 		os.Exit(1)
 	}
 
 	if apiKey == "" {
-		fmt.Printf("api-key must be provided\n")
+		logrus.Errorf("api-key must be provided\n")
 
 		os.Exit(1)
 	}
@@ -38,91 +45,122 @@ func main() {
 	// setup tcp server
 	listener, err := net.Listen("tcp", listenURI)
 	if err != nil {
-		fmt.Printf("Error listening: %s\n", err.Error())
+		logrus.Errorf("Error listening: %s\n", err.Error())
 
 		os.Exit(1)
 	}
 	defer listener.Close()
 
-	fmt.Printf("Listening on: %s\n", listenURI)
+	if pingDuration > 0 {
+		logrus.Infof("Send pings enabled with an interval %v seconds\n", pingDuration)
+	} else {
+		logrus.Infof("Send pings disabled\n")
+	}
+
+	logrus.Infof("Listening on: %s\n", listenURI)
 
 	ctx, cancel := context.WithCancel(context.Background())
 
-	go func() {
-		defer cancel()
+	defer cancel()
 
-		for {
-			tcpConn, err := listener.Accept()
-			if err != nil {
-				fmt.Printf("Error accepting: %s\n", err.Error())
+	for {
+		tcpConn, err := listener.Accept()
+		if err != nil {
+			logrus.Errorf("Error accepting: %s\n", err.Error())
 
-				return
-			}
+			return
+		}
 
-			fmt.Printf("Received message from %s to %s\n", tcpConn.RemoteAddr(), tcpConn.LocalAddr())
+		logrus.Debugf("(Connection from %s to %s) Starting\n", tcpConn.RemoteAddr(), tcpConn.LocalAddr())
 
-			wsConn, _, err := websocket.DefaultDialer.Dial(cloudURI, http.Header{
-				"Authorization": []string{
-					"Bearer " + apiKey,
-				},
-				"X-Application-ID": []string{applicationID},
-			})
+		wsConn, _, err := websocket.DefaultDialer.Dial(cloudURI, http.Header{
+			"Authorization": []string{
+				"Bearer " + apiKey,
+			},
+			"X-Application-ID": []string{applicationID},
+		})
 
-			if err != nil {
-				fmt.Printf("Error dial into edge tunneling service: %s\n, cloudURI - %s\n", err.Error(), cloudURI)
+		if err != nil {
+			logrus.Errorf("(Connection from %s to %s) Error dial into edge tunneling service: %s\n, cloudURI - %s\n", tcpConn.RemoteAddr(), tcpConn.LocalAddr(), err.Error(), cloudURI)
 
-				return
-			}
+			return
+		}
 
-			ctx, cancel := context.WithCancel(ctx)
+		connCtx, connCancel := context.WithCancel(ctx)
 
-			go func() {
-				for {
+		go func() {
+			defer connCancel()
+
+			chanBytes := make(chan []byte, 4096)
+
+			go func () {
+				defer connCancel()
+
+				for{
 					bytes := make([]byte, 4096)
 					length, err := tcpConn.Read(bytes)
 					if err != nil {
-						fmt.Printf("failed to read bytes from tcp connection: %s\n", err.Error())
+						logrus.Errorf("(Connection from %s to %s) failed to read bytes from tcp connection: %s\n", tcpConn.RemoteAddr(), tcpConn.LocalAddr(), err.Error())
 
-						break
+						return
 					}
+					chanBytes <- bytes[:length]
+				}
+			}()
 
+			c := time.Tick(time.Duration(pingDuration) * time.Second)
+			logrus.Infof("PingDuration :%v\n", pingDuration)
+
+			for {
+				select {
+				case <- connCtx.Done():
+					return
+				case bytes := <- chanBytes:
+					length := len(bytes)
 					if err := wsConn.WriteMessage(websocket.BinaryMessage, bytes[:length]); err != nil {
-						fmt.Printf("failed to write binary message to websocket connection: %s\n", err.Error())
+							logrus.Errorf("(Connection from %s to %s) failed to write binary message to websocket connection: %s\n", tcpConn.RemoteAddr(), tcpConn.LocalAddr(), err.Error())
 
-						break
+						return
 					}
 
-					fmt.Printf("write %d bytes of data to websocket connection\n", length)
+					logrus.Debugf("(Connection from %s to %s) write %d bytes of data to websocket connection\n", tcpConn.RemoteAddr(), tcpConn.LocalAddr(), length)
+
+				case <-c:
+					if err := wsConn.WriteControl(websocket.PingMessage, []byte(""), time.Now().Add(time.Second)); err != nil {
+						logrus.Errorf("(Connection from %s to %s) error sending ping %s\n", tcpConn.RemoteAddr(), tcpConn.LocalAddr(), err)
+
+						return
+					}
+
+					logrus.Debugf("(Connection from %s to %s) sent ping\n", tcpConn.RemoteAddr(), tcpConn.LocalAddr())
+				}
+			}
+		}()
+
+		go func() {
+			defer connCancel()
+
+			for {
+				_, data, err := wsConn.ReadMessage()
+				if err != nil {
+					logrus.Errorf("(Connection from %s to %s) failed to read binary message from websocket connection: %s\n", tcpConn.RemoteAddr(), tcpConn.LocalAddr(), err.Error())
+
+					break
 				}
 
-				cancel()
-			}()
+				length, err := tcpConn.Write(data)
+				if err != nil {
+					logrus.Errorf("(Connection from %s to %s) failed to write bytes to tcp connection: %s\n", tcpConn.RemoteAddr(), tcpConn.LocalAddr(), err.Error())
 
-			go func() {
-				for {
-					_, data, err := wsConn.ReadMessage()
-					if err != nil {
-						fmt.Printf("failed to read binary message from websocket connection: %s\n", err.Error())
-
-						break
-					}
-
-					length, err := tcpConn.Write(data)
-					if err != nil {
-						fmt.Printf("failed to write bytes to tcp connection: %s\n", err.Error())
-
-						break
-					}
-
-					fmt.Printf("write %d bytes of data back to local tcp connection\n", length)
+					break
 				}
 
-				cancel()
-			}()
+				logrus.Debugf("(Connection from %s to %s) write %d bytes of data back to local tcp connection\n", tcpConn.RemoteAddr(), tcpConn.LocalAddr(), length)
+			}
 
-			<-ctx.Done()
-		}
-	}()
+		}()
 
-	<-ctx.Done()
+		logrus.Infof("(Connection from %s to %s) Dispatched\n", tcpConn.RemoteAddr(), tcpConn.LocalAddr())
+	}
+
 }
